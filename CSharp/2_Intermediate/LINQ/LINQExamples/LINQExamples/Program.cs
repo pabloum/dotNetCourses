4b27995d0700a6941cf7e05a5e3022b229384192
Pablo Uribe Montoya <pablo.um00@gmail.com>
2020-05-13 09:12:51 -0500

    Introduction to LINQ. Initial steps

---
+++ CSharp/2_Intermediate/LINQ/LINQExamples/LINQExamples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Introduction
+{
+    class Program
+    {
+        
+        // This goes to a path, and then brings a list in descendent order os the files in that path
+        public static string ShowFilesWithoutLinq(string path)
+        {
+            return "";
+        }
+
+        // This goes to a path, and then brings a list in descendent order os the files in that path
+        public static string ShowFilesWithLinq(string path)
+        {
+            return "";
+        }
+
+        static void Main(string[] args)
+        {
+            string path = @"C:/windows";
+            var response1 = ShowFilesWithoutLinq(path);
+            var response2 = ShowFilesWithLinq(path);
+        }
+    }
+}
--- CSharp/2_Intermediate/LINQ/Notes.md
+++ CSharp/2_Intermediate/LINQ/Notes.md
@@ -10,7 +10,19 @@ LINQ - Language Integrated Query.
 1. INTRODUCTION.
 *****************************
 
+How to work with data 'in the past'?
+  -> Depends on where the data is.
+      + In memory? -> Through Object Data - Algorithms and Generics.
+      + Relational Data -> ADO.Net and SQL
+      + XML Data -> XmlDocument and XPath/ESLT
 
+Well, LINQ offers a consistent API for all these different data sources.
+
+LINQ gives us strongly typed, compile checks on queries that can execute against
+in-memory data, relational data, and XML Data.
+
+What can I access?
+  - Objects, MongoDB, CSV Files, File System, SQL DB, JSON, HL7 XML and more!!
 
 *****************************
 2. LINQ and C#.
