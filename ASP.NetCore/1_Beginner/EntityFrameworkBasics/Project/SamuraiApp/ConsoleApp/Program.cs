1e57ec1a922372e187d099db00be5ba19d833d68
Pablo Uribe Montoya <pablo.um00@gmail.com>
2020-08-20 16:58:21 -0500

    Add experiment test with using() statement in EF Core demo

--- ASP.NetCore/1_Beginner/EntityFrameworkBasics/Project/SamuraiApp/ConsoleApp/Program.cs
+++ ASP.NetCore/1_Beginner/EntityFrameworkBasics/Project/SamuraiApp/ConsoleApp/Program.cs
@@ -7,34 +7,45 @@ namespace ConsoleApp
 {
     internal class Program
     {
-        private static SamuraiContext context = new SamuraiContext();
+        //private static SamuraiContext context = new SamuraiContext();
 
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            context.Database.EnsureCreated();
+            using (var context = new SamuraiContext())
+            {
+                context.Database.EnsureCreated();
+            }
+
             GetSamurais("Before Add: ");
             AddSamurai();
             GetSamurais("After Add: ");
+
             Console.Write("Press any key");
             Console.ReadKey(); 
         }
             
         private static void AddSamurai()
         {
-            var samurai = new Samurai { Name = "Pablo" };
-            context.Samurais.Add(samurai);
-            context.SaveChanges();
+            using (var context = new SamuraiContext())
+            {
+                var samurai = new Samurai { Name = "Vero" };
+                context.Samurais.Add(samurai);
+                context.SaveChanges();
+            }
         }
 
         private static void GetSamurais(string text)
         {
-            var samurais = context.Samurais.ToList();
-            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
-            foreach (var samurai in samurais)
+            using (var context = new SamuraiContext())
             {
-                Console.WriteLine(samurai.Name);
+                var samurais = context.Samurais.ToList();
+                Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+                foreach (var samurai in samurais)
+                {
+                    Console.WriteLine(samurai.Name);
+                }
             }
         }
     }
