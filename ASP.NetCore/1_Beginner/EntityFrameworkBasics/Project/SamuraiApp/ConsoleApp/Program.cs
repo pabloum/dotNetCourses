84880358cdb839247aef091d7d8b45a24ea9f927
Pablo Uribe Montoya <pablo.um00@gmail.com>
2020-08-20 16:54:41 -0500

    Add first EFCore demo in a Console App

--- ASP.NetCore/1_Beginner/EntityFrameworkBasics/Project/SamuraiApp/ConsoleApp/Program.cs
+++ ASP.NetCore/1_Beginner/EntityFrameworkBasics/Project/SamuraiApp/ConsoleApp/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using SamuraiApp.Domain;
+using SamuraiApp.Data;
+using System.Linq;
 
 namespace ConsoleApp
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static SamuraiContext context = new SamuraiContext();
+
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            context.Database.EnsureCreated();
+            GetSamurais("Before Add: ");
+            AddSamurai();
+            GetSamurais("After Add: ");
+            Console.Write("Press any key");
+            Console.ReadKey(); 
+        }
+            
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Pablo" };
+            context.Samurais.Add(samurai);
+            context.SaveChanges();
+        }
+
+        private static void GetSamurais(string text)
+        {
+            var samurais = context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
         }
     }
 }
